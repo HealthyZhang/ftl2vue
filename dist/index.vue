239a9e9c5b0ef964d92052614fe560197211267f
healthy <zjkhealthy@163.com>
2022-08-14 17:28:52 +0800

    add dist

---
+++ dist/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+<script>
+  export default {
+    components: {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    beforeDestory() {
+
+    },
+    methods: {
+      fn1() {
+
+      },
+      fn2() {
+
+      },
+    }
+  }
+</script>
+<style scoped>
+
+
+</style>
