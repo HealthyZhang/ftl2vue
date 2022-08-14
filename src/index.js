d1dd3e1203fda3f8cb4fbd13ff78a03fe0a68846
healthy <zjkhealthy@163.com>
2022-08-14 17:49:19 +0800

    first complete

--- src/index.js
+++ src/index.js
@@ -3,22 +3,52 @@ const fse = require('fs-extra');
 
 
 
-let ftlPath = path.resolve(__dirname, '../ftl/ftl2.ftl');
-let vuePath = path.resolve(__dirname, '../dist/vue.vue');
+let ftlPath = path.resolve(__dirname, '../ftl');
 
 let ftlContent, vueContent, vueTemplate, vueScript, vueStyle;
 createVueTemplate()
-fse.readFile(ftlPath, (err, data) => {
-  if (err) throw err;
-  // console.log(data.toString())
-  ftlContent = data.toString();
-  exegesis()
-  moveStyle()
-  moveScript()
-  moveHtml()
-})
 
-function moveHtml() {
+readFiles(ftlPath)
+
+function readFiles(filePath) {
+  //根据文件路径读取文件，返回文件列表
+  fse.readdir(filePath, function (err, files) {
+    if (err) {
+      console.warn(err)
+    } else {
+      //遍历读取到的文件列表
+      files.forEach(function (filename) {
+        //获取当前文件的绝对路径
+        var filedir = path.join(filePath, filename);
+        //根据文件路径获取文件信息，返回一个fse.Stats对象
+        fse.stat(filedir, function (eror, stats) {
+          if (eror) {
+            console.warn('获取文件stats失败');
+          } else {
+            var isFile = stats.isFile(); //是文件
+            var isDir = stats.isDirectory(); //是文件夹
+            if (isFile) {
+              // console.log(filedir); // 读取文件内容
+              ftlContent = fse.readFileSync(filedir, 'utf-8');
+              // console.log(content);
+              let vuePath = filedir.replace(/\\ftl\\/, '\\dist\\')
+              vuePath = vuePath.replace(/\.ftl/, '\.vue')
+              exegesis()
+              moveStyle()
+              moveScript()
+              moveHtml(vuePath)
+            }
+            if (isDir) {
+              readFiles(filedir); //递归，如果是文件夹，就继续遍历该文件夹下面的文件
+            }
+          }
+        })
+      });
+    }
+  });
+}
+
+function moveHtml(vuePath) {
   let html = ftlContent.match(/<body>(\d|\D)*\s+<\/body>/gi);
   html = html[0]
   // // 替换if语句
@@ -43,10 +73,10 @@ function moveHtml() {
   html = html.replace(/(>|\s)\$\{([\w.]+)\!?\}/gmi, '$1{{$2}}');
 
   vueTemplate = vueTemplate.replace('ht', html.substring(5, html.length - 7));
-  toVueFile()
+  toVueFile(vuePath)
 }
 
-function toVueFile() {
+function toVueFile(vuePath) {
   let content = vueTemplate + vueScript + vueStyle;
   fse.outputFile(vuePath, content).then(res => {
     console.log('outPutSuccess!', res)
