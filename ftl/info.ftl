0889ada27b85fe5492325d0a023f86d83603e6b4
zhangjiankang <zhangjiankang@glaer.com>
2024-01-31 11:29:12 +0800

    1

---
+++ ftl/info.ftl
@@ -0,0 +1,10 @@
+﻿ 
+<body  style="background-color:#f5f5f9;">
+ 
+    <div class="cas_head" id="cas_head">
+        <#include "/public/topnav.ftl">
+        <#include "/public/headnav_shop.ftl">
+    </div>
+ 
+</body> 
+ 
--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const fse = require('fs-extra');
 
 let ftlPath = path.resolve(__dirname, '../ftl');
 
+// vue模板变量；
 let ftlContent, vueContent, vueTemplate, vueScript, vueStyle;
-createVueTemplate()
 
 readFiles(ftlPath)
 
@@ -14,7 +14,7 @@ function readFiles(filePath) {
   //根据文件路径读取文件，返回文件列表
   fse.readdir(filePath, function (err, files) {
     if (err) {
-      console.warn(err)
+      console.warn("文件读取错误！错误内容：", err)
     } else {
       //遍历读取到的文件列表
       files.forEach(function (filename) {
@@ -28,15 +28,17 @@ function readFiles(filePath) {
             var isFile = stats.isFile(); //是文件
             var isDir = stats.isDirectory(); //是文件夹
             if (isFile) {
+              createVueTemplate()
+
               // console.log(filedir); // 读取文件内容
               ftlContent = fse.readFileSync(filedir, 'utf-8');
               // console.log(content);
               let vuePath = filedir.replace(/\\ftl\\/, '\\dist\\')
               vuePath = vuePath.replace(/\.ftl/, '\.vue')
-              exegesis()
-              moveScript()
+              exegesis(vuePath)
               moveHtml(vuePath)
               moveStyle(vuePath)
+              moveScript(vuePath)
             }
             if (isDir) {
               readFiles(filedir); //递归，如果是文件夹，就继续遍历该文件夹下面的文件
@@ -47,37 +49,56 @@ function readFiles(filePath) {
     }
   });
 }
+// HTML 修改，待扩展；
+function moveHtml(vuePath) {
+  let html = ftlContent.match(/<body(\d|\D)*\s+<\/body>/gi);
+  try {
+    html = html[0]
+    addData(html, vuePath)
+    // // 替换if语句
+    html = html.replace(/<\/#if>/gim, '</fragment>');
+    html = html.replace(/<#else>/gim, '</fragment><fragment v-else>');
 
-function moveHtml() {
-  let html = ftlContent.match(/<body>(\d|\D)*\s+<\/body>/gi);
-  html = html[0]
-  // // 替换if语句
-  html = html.replace(/<\/#if>/gim, '</fragment>');
-  html = html.replace(/<#else>/gim, '</fragment><fragment v-else>');
+    html = html.replace(/<#if(\S|\s(?!(<)))*>/gim, function (math) {
+      return '<fragment v-if="' + math.substring(5, math.length - 1) + '">'
+    })
 
-  html = html.replace(/<#if(\S|\s(?!(<)))*>/gim, function (math) {
-    return '<fragment v-if="' + math.substring(5, math.length - 1) + '">'
-  })
-
-  html = html.replace(/<#elseif(\S|\s(?!(<)))*>/gim, function (math) {
-    return '</fragment><fragment v-else-if="' + math.substring(5, math.length - 1) + '">'
-  })
-  // 替换for循环；
-  html = html.replace(/<\/#list>/gim, '</fragment>');
-  html = html.replace(/<#list\s(\S*)\sas\s(\S*)>/gim, '<fragment v-for="($2,index) in $1" :key="index">')
+    html = html.replace(/<#elseif(\S|\s(?!(<)))*>/gim, function (math) {
+      return '</fragment><fragment v-else-if="' + math.substring(8, math.length - 1) + '">'
+    })
+    // 替换for循环；
+    html = html.replace(/<\/#list>/gim, '</fragment>');
+    html = html.replace(/<#list\s(\S*)\sas\s(\S*)>/gim, '<fragment v-for="($2,index) in $1" :key="index">')
 
 
-  // 解析${} 
-  // 标签内不携带字符的模板；
-  html = html.replace(/(\s)(\w+)=\"\$\{([\w.]+)\!?\}/gmi, "$1:$2=\"" + "$3")
-  // 标签内携带字符模板；
-  html = html.replace(/(\s)(\w+)=\"((\S(?!>))*)\$\{([\w.]+)\!?(\?c)?\}/gmi, "$1:$2=" + "\"'$3" + "'" + "+ $5")
-  // 标签内多个query属性； 
-  html = html.replace(/\$\{([\w.]+)\!?(\?c)?\}\&/gmi, "'+$1+'")
-  // 非标签内模板；
-  html = html.replace(/(>|\s)\$\{([\w.]+)\!?(\?c)?\}/gmi, '$1{{$2}}');
+    // 解析${} 
+    // 标签内不携带字符的模板；
+    html = html.replace(/(\s)(\w+)=\"\$\{([\w.]+)\!?\}/gmi, "$1:$2=\"" + "$3")
+    // 标签内携带字符模板；
+    html = html.replace(/(\s)(\w+)=\"((\S(?!>))*)\$\{([\w.]+)\!?(\?c)?\}/gmi, "$1:$2=" + "\"'$3" + "'" + "+ $5")
+    // 标签内多个query属性； 
+    html = html.replace(/\$\{([\w.]+)\!?(\?c)?\}\&/gmi, "'+$1+'")
+    // 非标签内模板；
+    html = html.replace(/(>|\s)\$\{([\w.]+)\!?(\?c)?\}/gmi, '$1{{$2}}');
+    vueTemplate = vueTemplate.replace('ht', html.substring(5, html.length - 7));
 
-  vueTemplate = vueTemplate.replace('ht', html.substring(5, html.length - 7));
+  } catch (e) {
+    html = ''
+    console.log('moveHtmlError:', e, vuePath)
+  }
+}
+
+function addData(html, vuePath) {
+  dataArr = html.match(/\$\{([\w.]+)\!?(\?c)?\}/gmi);
+  let dt = '';
+  try {
+    dataArr && dataArr.forEach(item => {
+      dt += item.replace(/(\$\{)|(\!?\})|((\?c)?\})/gim, '').split('.')[0] + ": '',"
+    })
+    vueScript = vueScript.replace('dt', dt)
+  } catch (e) {
+    console.log('addDataerr:', e, vuePath)
+  }
 
 }
 
@@ -86,82 +107,95 @@ function toVueFile(vuePath) {
   fse.outputFile(vuePath, content).then(res => {
     console.log('outPutSuccess!', res)
   }).catch(err => {
-    console.log('outPFial:', err)
+    console.log('outPFial:', err, vuePath)
   })
 }
-
-function moveScript() {
-  let scriptArr1 = ftlContent.match(/<script>(\S|\s(?!(<script|src)))*<\/script>/g);
-  let mtd1 = '';
-  scriptArr1 && scriptArr1.forEach(string => {
-    mtd1 += string.substring(8, string.length - 9)
-  });
-  vueScript = vueScript.replace(/mtd1/g, mtd1)
-  let scriptArr2 = ftlContent.match(/<script\stype(\S|\s(?!(<script|src)))*<\/script>/g);
-  let mtd2 = ''
-  scriptArr2 && scriptArr2.forEach(string => {
-    mtd2 += string.substring(32, string.length - 9)
-  });
-  vueScript = vueScript.replace(/mtd2/g, mtd2)
+// 调出js语句；
+function moveScript(vuePath) {
+  try {
+    let scriptArr1 = ftlContent.match(/<script>(\S|\s(?!(<script|src)))*<\/script>/g);
+    let mtd1 = '';
+    scriptArr1 && scriptArr1.forEach(string => {
+      mtd1 += string.substring(8, string.length - 9)
+    });
+    vueScript = vueScript.replace(/mtd1/g, mtd1)
+    let scriptArr2 = ftlContent.match(/<script\stype(\S|\s(?!(<script|src)))*<\/script>/g);
+    let mtd2 = ''
+    scriptArr2 && scriptArr2.forEach(string => {
+      mtd2 += string.substring(30, string.length - 9)
+    });
+    vueScript = vueScript.replace(/mtd2/g, mtd2)
+    toVueFile(vuePath)
+  } catch (e) {
+    console.log('moveScriptErr;', e, vuePath)
+  }
 }
 
-
+// 拼接样式；
 function moveStyle(vuePath) {
   let ftlStyleArr = ftlContent.match(/<style(\S|(\s(?!<style)))*<\/style>/g)
   let styleString;
   try {
     styleString = ftlStyleArr.join('\r\n');
     styleString = styleString.replace(/<style>|<\/style>|<style type="text\/css">/g, '')
+    vueStyle = vueStyle.replace('stl', styleString);
   } catch (e) {
-    console.log('moveStyleerr');
+    console.log('moveStyleerr', e, vuePath);
+    // console.log('moveStyleerr', vuePath);
   }
-  vueStyle = vueStyle.replace('stl', styleString);
-  toVueFile(vuePath)
 }
 
+// 创建vue基础模板；
 function createVueTemplate() {
   vueTemplate = '<template>' +
+    '\n' +
     '  <div class="">' +
+    '\n' +
     'ht' +
+    '\n' +
     '  </div>' +
-    '</template>' +
+    '\n' +
+    '</template>' + '\n' +
     '';
   vueScript = '<script>' +
+    '\n' +
     '  export default {' +
-    '    components:  {' +
+    '\n' +
+    '    components:  {' + '\n' +
     ' ' +
-    '     },' +
-    '     data() {' +
-    '       return {' +
-    ' dt' +
-    '       }' +
-    '     },' +
-    '     created() {' +
-    ' ' +
-    '     },' +
-    '     mounted() {' +
-    ' ' +
-    '     },' +
-    '     beforeDestory() {' +
-    ' ' +
-    '     },' +
-    '     methods: {' +
-    '       fn1(){' +
-    '           mtd1' +
-    '        },' +
-    '       fn2(){' +
-    '           mtd2' +
-    '        },' +
-    '     }' +
-    '   }' +
-    ' </script>' +
+    '     },' + '\n' +
+    '     data() {' + '\n' +
+    '       return {' + '\n' +
+              ' dt' + '\n' +
+    '       }' + '\n' +
+    '     },' + '\n' +
+    '     created() {' + '\n' +
+    ' ' + '\n' +
+    '     },' + '\n' +
+    '     mounted() {' + '\n' +
+    ' ' + '\n' +
+    '     },' + '\n' +
+    '     beforeDestory() {' + '\n' +
+    ' ' + '\n' +
+    '     },' + '\n' +
+    '     methods: {' + '\n' +
+    '       fn1(){' + '\n' +
+    '           mtd1' + '\n' +
+    '        },' + '\n' +
+    '       fn2(){' + '\n' +
+    '           mtd2' + '\n' +
+    '        },' + '\n' +
+    '     }' + '\n' +
+    '   }' + '\n' +
+    ' </script>' + '\n' +
     '';
-  vueStyle = ' <style scoped>' +
-    ' ' +
-    ' stl' +
+  vueStyle = ' <style scoped>' + '\n' +
+    ' ' + '\n' +
+    ' stl' + '\n' +
     ' </style>'
 }
 
+// 修改注释；
 function exegesis() {
   ftlContent = ftlContent.replace(/<#--/g, '<!--')
 }
